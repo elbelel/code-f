35cd0cd04d175f7bcac16cabd56d8558afe98fe9
reclursive <imafraidofferrets@gmail.com>
2024-06-05 00:52:27 -0400

    landing page partially organized, tabs set, image, searchbar in donations tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './App.scss';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import './App.css'
 
 function App() {
@@ -8,23 +12,33 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Fund Family</h1>
+      <img src="https://media.discordapp.net/attachments/1237848613325901868/1244318779534610452/family-removebg-preview.png?ex=666133eb&is=665fe26b&hm=3853716c79075cdc300573a28452e44d35072d5aa279655de53446a8c65d01e7&=&format=webp&quality=lossless&width=200&height=200" alt="arms hug heart"/>
+          <Tabs
+          defaultActiveKey="profile"
+          id="uncontrolled-tab-example"
+          className="mb-3"
+        >
+          <Tab eventKey="donations" title="Donations">
+          <Form.Control type="text" placeholder="Search"/>
+            Tab content for donations
+            <div className="card">
+          <Button onClick={() => setCount((count) => count + 1)}>
+            Donate {count}
+          </Button>
+            <p>
+              Donations are <code>nonrefundable</code> 
+            </p>
+          </div>
+          </Tab>
+          <Tab eventKey="senators" title="Senators">
+            Tab content for Profile
+          </Tab>
+          <Tab eventKey="about" title="How we work">
+            Tab content for Contact
+          </Tab>
+        </Tabs>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
